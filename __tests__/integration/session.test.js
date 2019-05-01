0c2d9bd58fedba37b31bef19f4045af5c0597125
João Henrique <joaoh3326@gmail.com>
2019-05-01 08:14:52 -0300

    criado testes e features para session

---
+++ __tests__/integration/session.test.js
@@ -0,0 +1,38 @@
+const request = require("supertest");
+const app = require("../../src/app");
+const truncate = require("../utils/truncate");
+const { User } = require("../../src/app/models");
+
+describe("Authentication", () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
+  it("should be able to authenticate with valid credentials", async () => {
+    const user = await User.create({
+      name: "João",
+      email: "joao@email.com",
+      password: "123123"
+    });
+
+    const response = await request(app)
+      .post("/sessions")
+      .send({ email: user.email, password: "123123" });
+
+    expect(response.status).toBe(200);
+  });
+
+  it("should not be able to authenticate with invalid credentials", async () => {
+    const user = await User.create({
+      name: "João",
+      email: "joao@email.com",
+      password: "123456"
+    });
+
+    const response = await request(app)
+      .post("/sessions")
+      .send({ email: user.email, password: "123123" });
+
+    expect(response.status).toBe(401);
+  });
+});
